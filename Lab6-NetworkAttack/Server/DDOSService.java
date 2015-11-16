3d43d873017f6427665e1fced59f717f336be1c9
LuchFilip <filipluch@gmail.com>
2015-11-16 15:57:31 +0200

    removed lab6 code

--- Lab6-NetworkAttack/Server/DDOSService.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.*;
-// DDOSService Interface
-// Interface for a RMI service that will actually attack on a target machine
-public interface DDOSService extends java.rmi.Remote
-{
- public String attack ( )
-  throws RemoteException;
-}
